005a4d6fcce49f7264ef21a11870ed1f83cc1ef6
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2026-08-10 22:36:21 +0530

    #cleaning

--- plugins/randomsticker.py
+++ plugins/randomsticker.py
@@ -4,7 +4,6 @@
 #  Plugin Name:    randomsticker
 #  Version:        1.0.0
 #  Author:         CipherElite Dev (@rishabhops)
-#  Ported from:    CatPlugins-main
 #  License:        MIT
 #
 #  Commands:       .cat, .dab, .brain, .pat
