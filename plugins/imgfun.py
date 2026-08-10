9bd1a6f2874561b95143de36896d06e635751319
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2026-08-10 22:31:38 +0530

    #cleaning

--- plugins/imgfun.py
+++ plugins/imgfun.py
@@ -4,7 +4,6 @@
 #  Plugin Name:    imgfun
 #  Version:        1.0.0
 #  Author:         CipherElite Dev (@rishabhops)
-#  Ported from:    CatPlugins-main
 #  License:        MIT
 #
 #  Commands:       .imirror, .irotate, .iresize, .square, .dotify
