f6c49742e359171f433763d57352d2ab5eeaa012
rishabh anand <156223377+rishabhops@users.noreply.github.com>
2026-08-10 22:25:18 +0530

    #cleaning

--- plugins/art.py
+++ plugins/art.py
@@ -4,7 +4,6 @@
 #  Plugin Name:    catart
 #  Version:        1.0.0
 #  Author:         CipherElite Dev (@rishabhops)
-#  Ported from:    CatPlugins-main
 #  License:        MIT
 #
 #  Commands:       .hello, .killer
